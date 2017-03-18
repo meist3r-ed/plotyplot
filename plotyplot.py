5685cf05b9b17c37574618bee90ac52f45e86dd1
meist3r-ed <g.gaiax@gmail.com>
2017-03-17 23:41:37 -0300

    New function added, gitignore fixed

--- plotyplot.py
+++ plotyplot.py
@@ -7,13 +7,16 @@ def valRGB(seed, num):
     x = (255 * num)/seed
     return num
 
-def function1(image, size):
+def function(image, size, q, funcnum):
     for x in range(0, size):
         for y in range(0, size):
             xRGB = valRGB(size, x)
             yRGB = valRGB(size, y)
-            sumRGB = xRGB + yRGB
-            image[x,y] = (sumRGB, sumRGB, sumRGB)
+            if(funcnum == 1):
+                resRGB = xRGB + yRGB
+            if(funcnum == 2):
+                resRGB = np.absolute(np.sin(xRGB/q) * 255)
+            image[x,y] = (resRGB, resRGB, resRGB)
 
     return image
 
@@ -22,9 +25,13 @@ filename = input("Type in the output filename (w/o filetype): ")
 filename += ".png"
 size = input("Type in the image resolution (in px): ")
 size = int(size)
+q = input("Type in the frequency parameter (Q): ")
+q = int(q)
+
+funcnum = 2
 
 output = np.zeros((size, size, 3), np.uint8)
-output = function1(output, size)
+output = function(output, size, q, funcnum)
 
 cv2.imshow('output', output)
 cv2.imwrite(filename, output)
