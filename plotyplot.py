dd88f1bddad6bcc5d7a896613265e6a50f7daabf
meist3r-ed <g.gaiax@gmail.com>
2017-03-18 04:04:53 -0300

    Functions fully implemented & working, further documentation and testing needed

--- plotyplot.py
+++ plotyplot.py
@@ -1,40 +1,77 @@
-#   P L O T Y P L O T v0.0   #
+#   P L O T Y P L O T v0.75  #
+# random version numbers yay #
 # Gustavo Santiago,  8937416 #
 import cv2
 import numpy as np
 
+#Transforms num to RGB scale, from which seed is equivalent 255
 def valRGB(seed, num):
     x = (255 * num)/seed
     return num
 
-def function(image, size, q, funcnum):
+#Program functions
+def calcRGB(xRGB, yRGB, q, funcnum):
+    resRGB = 0
+    if(funcnum == 1):
+        #f(x, y) = (x + y)
+        resRGB = xRGB + yRGB
+    elif(funcnum == 2):
+        #f(x, y) = |sin(x, q) * 255|
+        resRGB = np.absolute(np.sin(xRGB/q) * 255)
+    elif(funcnum == 3):
+        #f(x, y) = |(x/q)^2 + 2*(y/q)^2| * 255
+        resRGB = np.absolute(np.power(xRGB/q, 2) + 2 * np.power(yRGB/q, 2))/255
+    elif(funcnum == 4):
+        #f(x, y) = rand(0, 255)
+        resRGB = np.random.randint(0, 255)
+    return resRGB
+
+#Functions' loop, iterating through the image's pixels
+def functionLoop(image, size, q, funcnum):
     for x in range(0, size):
         for y in range(0, size):
+            #Changing X and Y to the RGB scale...
             xRGB = valRGB(size, x)
             yRGB = valRGB(size, y)
-            if(funcnum == 1):
-                resRGB = xRGB + yRGB
-            if(funcnum == 2):
-                resRGB = np.absolute(np.sin(xRGB/q) * 255)
+            #Calculating f(x, y)...
+            resRGB = calcRGB(xRGB, yRGB, q, funcnum)
+            #Filling the pixels...
             image[x,y] = (resRGB, resRGB, resRGB)
-
     return image
 
-print("### P L O T Y P L O T v0.0 ###")
+#main#
+#----------------------------------------------------------------------#
+#Splashscreen :D
+print("### P L O T Y P L O T v0.75 ###")
+
 filename = input("Type in the output filename (w/o filetype): ")
+if(len(filename) == 0):
+    filename = "output"
 filename += ".png"
+
 size = input("Type in the image resolution (in px): ")
 size = int(size)
-q = input("Type in the frequency parameter (Q): ")
-q = int(q)
+if(size <= 0):
+    print("Invalid size :(")
+else:
+    funcnum = input("Type in the function number (1-4): ")
+    funcnum = int(funcnum)
+    if(funcnum < 1 or funcnum > 4):
+        print("Invalid function :(")
+    else:
+        if(funcnum != 1 and funcnum != 4):
+            q = input("Type in the frequency parameter (Q): ")
+            q = int(q)
+        else:
+            q = 1
 
-funcnum = 2
+        output = np.zeros((size, size, 3), np.uint8)
+        output = functionLoop(output, size, q, funcnum)
 
-output = np.zeros((size, size, 3), np.uint8)
-output = function(output, size, q, funcnum)
+        cv2.imshow('plotyplot_output', output)
+        cv2.imwrite(filename, output)
 
-cv2.imshow('output', output)
-cv2.imwrite(filename, output)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+print("Closing program...")
+#----------------------------------------------------------------------#
